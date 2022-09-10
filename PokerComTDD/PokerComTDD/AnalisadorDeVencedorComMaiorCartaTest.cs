09eba741211e3edeee0873e16f3846011530319f
Lucas macedo da Silva <luckasmacedo2@gmail.com>
2022-09-10 15:07:11 -0300

    - Criação do caso de teste de jogo de póker; - Implementação dos casos de teste para o jogo de póker.

---
+++ PokerComTDD/PokerComTDD/AnalisadorDeVencedorComMaiorCartaTest.cs
@@ -0,0 +1,24 @@
+﻿using Xunit;
+
+namespace PokerComTDD
+{
+    public class AnalisadorDeVencedorComMaiorCartaTest
+    {
+        [Theory]
+        [InlineData("2O,4C,3P,6C,7C", "3O,5C,2E,9C,7P", "Segundo jogador")]
+        [InlineData("3O,5C,2E,9C,7P", "2O,4C,3P,6C,7C", "Primeiro jogador")]
+        [InlineData("3O,5C,2E,9C,7P", "2O,4C,3P,6C,10E", "Segundo jogador")]
+        [InlineData("3O,5C,2E,9C,VP", "2O,4C,3P,6C,AE", "Segundo jogador")]
+        public void DeveAnalisarQuandoJogadorTiverMaiorCarta(string cartasDoPrimeiroJogadorStr, string cartasDoSegundoJogadorStr, string vencedorEsperado)
+        {
+            var cartasDoPrimeiroJogador = cartasDoPrimeiroJogadorStr.Split(",").ToList();
+            var cartasDoSegundoJogador = cartasDoSegundoJogadorStr.Split(",").ToList();
+
+            var analisador = new AnalisadorDeVencedorComMaiorCarta();
+
+            var vencedor = analisador.Analisar(cartasDoPrimeiroJogador, cartasDoSegundoJogador);
+
+            Assert.Equal(vencedorEsperado, vencedor);
+        }
+    }
+}
---
+++ PokerComTDD/PokerComTDD/CartaTest.cs
@@ -0,0 +1,65 @@
+﻿using ExpectedObjects;
+using Xunit;
+
+namespace PokerComTDD
+{
+    public class CartaTest
+    {
+        [Theory]
+        [InlineData("A", "O", 14)]
+        [InlineData("10", "E", 10)]
+        [InlineData("2", "P", 2)]
+        public void DeveCriarUmaCarta(string valorDaCarta, string naipeDaCarta, int pesoDaCarta)
+        {
+            var cartaEsperada = new
+            {
+                Valor = valorDaCarta,
+                Naipe = naipeDaCarta,
+                Peso = pesoDaCarta
+            };
+
+            var carta = new Carta(cartaEsperada.Valor + cartaEsperada.Naipe);
+
+            cartaEsperada.ToExpectedObject().ShouldMatch(carta);
+        }
+
+        [Theory]
+        [InlineData("V", 11)]
+        [InlineData("D", 12)]
+        [InlineData("R", 13)]
+        [InlineData("A", 14)]
+        public void DeveCriarUmaCartaComPeso(string valorDaCarta, int pesoEsperado)
+        {            
+            var carta = new Carta(valorDaCarta + "E");
+
+            Assert.Equal(pesoEsperado, carta.Peso);
+        }
+
+        [Theory]
+        [InlineData("0")]
+        [InlineData("1")]
+        [InlineData("15")]
+        [InlineData("10000")]
+        [InlineData("-1")]
+        public void DeveValidarValorDaCarta(string valorDaCartaInvalida)
+        {
+
+            var mensagemDeErro = Assert.Throws<Exception>(() => new Carta(valorDaCartaInvalida + "O")).Message;
+            Assert.Equal("Valor da carta inválida", mensagemDeErro);
+        }
+
+        [Theory]
+        [InlineData("")]
+        [InlineData(null)]
+        [InlineData("G")]
+        [InlineData("10000")]
+        [InlineData("A")]
+        [InlineData("Z")]
+        public void DeveValidarNaipeDaCarta(string naipeDaCartaInvalido)
+        {
+            // Válidos: Ouro (O), Copa (H), Espada (S) e Paus (C)
+            var mensagemDeErro =  Assert.Throws<Exception>(() => new Carta("2" + naipeDaCartaInvalido)).Message;
+            Assert.Equal("Naipe inválido", mensagemDeErro);
+        }
+    }
+}
